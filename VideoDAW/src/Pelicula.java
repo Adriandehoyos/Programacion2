926e3013905466445fcd3fc6d0bfc7d829de6847
COMPU-S038\daw1 <adrianhoyos098@gmail.com>
2024-12-02 12:49:50 +0100

    lunes

--- VideoDAW/src/Pelicula.java
+++ VideoDAW/src/Pelicula.java
@@ -7,25 +7,27 @@ public class Pelicula {
 
     private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
 
-    private String codigo;
+    private int codigo;
     private String titulo;
     private Genero genero;
     private LocalDate FechaRegistro;
     private LocalDate FechaBaja;
     private LocalDateTime FechaAlquiler;
     private boolean isAlquilada;
+    private static int contador = 0;
 
-    public Pelicula(String codigo, String titulo, Genero genero){
-        this.codigo = codigo;
+    public Pelicula(String titulo, Genero genero){
         this.titulo = titulo;
         this.genero = genero;
         this.FechaRegistro = LocalDate.now();
         this.FechaBaja = LocalDate.now();
         this.FechaAlquiler = LocalDateTime.now();
+        this.codigo = contador;
+        contador++;
         
     }
 
-    public String getCodigo(){
+    public int getCodigo(){
         return this.codigo;
     }
 
